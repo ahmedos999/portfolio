ba5bda9a98451fee5a950dd0d67b6ccad99330b8
ahmed osman <ahmedos099912@gmail.com>
2023-09-27 16:51:14 +0200

    about page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import About from "./components/About";
 import Main from "./components/Main";
 import NavBar from "./components/NavBar";
 
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <NavBar></NavBar>
       <Main></Main>
+      <About></About>
     </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
+export default function About() {
+  return (
+    <div className='About'>
+        
+        <h2>About Me</h2>
+        <p>I am software engnieer who is passionate about coding and creating digital content, I always looking to learn about new technologies, love new challanges and ways to improve my skills</p>
+        <div className="indcators">
+        <CircularProgressbar value={80} maxValue={100} text='Dart' />
+        <CircularProgressbar value={60} maxValue={100} text='JS' />
+        <CircularProgressbar value={50} maxValue={100} text='Java' />
+        <CircularProgressbar value={80} maxValue={100} text='HTML' />
+        </div>
+    </div>
+  )
+}
