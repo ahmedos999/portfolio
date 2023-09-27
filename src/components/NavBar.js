1aa12a5838adc4272879266c78bfb671154d937d
ahmed osman <ahmedos099912@gmail.com>
2023-09-27 10:28:29 +0200

    navbar v1

---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <div className='nav'>
+        <div className="logo">
+            <h2>Ahmed</h2>
+            <h3>Sheikh</h3>
+        </div>
+        <div className="navlist">
+            <ul>
+                <li className='active'>Home</li>
+                <li>About</li>
+                <li>Skills</li>
+                <li>Contact me</li>
+            </ul>
+        </div>
+        <div className="cv">
+            <a href={require("../imgs/cv.pdf")} download="myFile">Download CV</a>
+        </div>
+    </div>
+  )
+}
