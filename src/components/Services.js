0beddd35b27edaf06ce9f3a009d02ee48df5a2e9
ahmed osman <ahmedos099912@gmail.com>
2023-09-28 13:55:48 +0200

    added icons

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,6 +1,11 @@
 import React from 'react'
-import img from '../imgs/mobile.png'
-import img2 from '../imgs/web.png'
+
+import { BiLogoFlutter } from "react-icons/bi";
+import { BiLogoReact } from "react-icons/bi";
+import { AiFillHtml5 } from "react-icons/ai";
+import { SiDynamics365 } from "react-icons/si";
+
+
 
 export default function Services() {
   return (
@@ -9,24 +14,24 @@ export default function Services() {
         <p>Preview and browse my distinguished services</p>
         <div className="cards">
             <div className="card">
-                <img src={img} alt="" />
+            <div className="icon"><BiLogoFlutter /></div>
                 <h4>Mobile Applcations</h4>
                 <p>Works on creating mobile application using the well known Flutter framework with the Dart programming language, with Bloc/provider for state managment</p>
             </div>
             <div className="card">
-                <img src={img2} alt="" />
+            <div className="icon"><BiLogoReact/></div>
                 <h4>Web Applcations</h4>
                 <p>Works on creating web application using the well known React framework with the Java Script programming language, with redux for state managment</p>
             </div>
             <div className="card">
-                <img src={img} alt="" />
+                <div className="icon"><AiFillHtml5 /></div>
                 <h4>Front-end development</h4>
-                <p>Works on Trasnforming each Design to code with HTML/CSS and javascript</p>
+                <p>Works on Trasnforming each UI/UX Design to code with HTML/CSS and javascript with responsive view for different devices</p>
             </div>
             <div className="card">
-                <img src={img} alt="" />
+            <div className="icon"><SiDynamics365 /></div>
                 <h4>ERP System</h4>
-                <p>Works as applcation engnieer for ERP modules specialized on microsoft dynamics 365 business central</p>
+                <p>Works as applcation engnieer for ERP modules specialized on microsoft dynamics 365 business central, for supply chain, sales and Warehouse managment</p>
             </div>
         </div>
     </div>
