704cba5949f190f8c439240dab5fd07a88420fee
ahmed osman <ahmedos099912@gmail.com>
2023-09-30 11:33:30 +0200

    editing my project

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import About from "./components/About";
 import Main from "./components/Main";
 import NavBar from "./components/NavBar";
 import Services from "./components/Services";
+import Work from "./components/Work";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Main></Main>
       <About></About>
       <Services></Services>
+      <Work></Work>
     </div>
   );
 }
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,14 +1,16 @@
 import React from 'react'
-import {img} from '../imgs/mobile mockup.png'
+import img from '../imgs/mobile mockup.png'
 export default function Work() {
   return (
     <div className='work'>
         <h2>Projects</h2>
         <div className="project-card">
             <img src={img} alt="" className="" />
+            <div>
             <h2>E-commarce Mobie application</h2>
             <p>Mobile applcation for shopping from your mobile applcation with all the necessary function</p>
             <a href="">Project Preview Link</a>
+            </div>
         </div>
     </div>
   )
