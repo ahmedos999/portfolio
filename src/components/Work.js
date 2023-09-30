11e5ce8c8ea04bb4a7fe967d71911b00939d0f02
ahmed osman <ahmedos099912@gmail.com>
2023-09-30 11:04:43 +0200

    created work section

---
+++ src/components/Work.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {img} from '../imgs/mobile mockup.png'
+export default function Work() {
+  return (
+    <div className='work'>
+        <h2>Projects</h2>
+        <div className="project-card">
+            <img src={img} alt="" className="" />
+            <h2>E-commarce Mobie application</h2>
+            <p>Mobile applcation for shopping from your mobile applcation with all the necessary function</p>
+            <a href="">Project Preview Link</a>
+        </div>
+    </div>
+  )
+}
