f14d9caa68d6e5f1cdcc3b01d7cf856eb06df21a
ahmed osman <ahmedos099912@gmail.com>
2023-10-01 12:03:47 +0200

    edit project section

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import img from '../imgs/mobile mockup.png'
+import img from '../imgs/test.png'
 export default function Work() {
   return (
     <div className='work'>
@@ -9,6 +9,7 @@ export default function Work() {
             <div>
             <h2>E-commarce Mobie application</h2>
             <p>Mobile applcation for shopping from your mobile applcation with all the necessary function</p>
+            <p>projects use Riverpod for state managment soluation with 10 diffrent screen, data is fetch from local json file in project</p>
             <a href="">Project Preview Link</a>
             </div>
         </div>
