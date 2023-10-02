004a1e02715e14d39e51929e4ecd067efa9455e1
ahmed osman <ahmedos099912@gmail.com>
2023-10-02 11:02:10 +0200

    adding second project

--- src/components/Work.js
+++ src/components/Work.js
@@ -13,6 +13,15 @@ export default function Work() {
             <a href="" className='button-50'>Project Preview Link</a>
             </div>
         </div>
+        <div className="project-card-l">
+            <img src={img} alt="" className="" />
+            <div>
+            <h2>E-commarce Mobie application</h2>
+            <p>Mobile applcation for shopping from your mobile applcation with all the necessary function</p>
+            <p>projects use Riverpod for state managment soluation with 10 diffrent screen, data is fetch from local json file in project</p>
+            <a href="" className='button-50'>Project Preview Link</a>
+            </div>
+        </div>
     </div>
   )
 }
