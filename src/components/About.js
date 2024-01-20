a493c2b0a529574afd3f9589a8d8bce6f5c8b91f
ahmed osman <ahmedos099912@gmail.com>
2024-01-20 22:55:43 +0200

    refactor:edit css class for indecators

--- src/components/About.js
+++ src/components/About.js
@@ -32,11 +32,11 @@ export default function About() {
         <h2>About Me</h2>
         <p>I am software engnieer who is passionate about coding and creating digital content, I always looking to learn about new technologies, love new challanges and ways to improve my skills</p>
         <div className="indcators">
-        <CircularProgressbar value={80} maxValue={100} text='Dart' />
-        <CircularProgressbar value={60} maxValue={100} text='Java Script' />
-        <CircularProgressbar value={50} maxValue={100} text='Java' />
-        <CircularProgressbar value={80} maxValue={100} text='HTML/CSS' />
-        <CircularProgressbar value={60} maxValue={100} text='SQL' />
+        <div><CircularProgressbar value={80} maxValue={100} text='Dart' /></div>
+        <div><CircularProgressbar value={60} maxValue={100} text='Java Script' /></div>
+        <div><CircularProgressbar value={50} maxValue={100} text='Java' /></div>
+        <div><CircularProgressbar value={80} maxValue={100} text='HTML/CSS' /></div>
+        <div><CircularProgressbar value={60} maxValue={100} text='SQL' /></div>
         </div>
     </div>
 
