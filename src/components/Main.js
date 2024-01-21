59354ccaf633b9052a0d0185ae674302664f250e
ahmed osman <ahmedos099912@gmail.com>
2024-01-21 14:50:36 +0200

    remove:main animation

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,7 +8,11 @@ import { motion } from 'framer-motion'
 
 export default function Main() {
 
-
+const imgVariants = {
+  hover:{
+    boxShadow:'rgba(15, 105, 136, 0.4) 5px 5px, rgba(3, 96, 117, 0.3) 10px 10px, rgba(33, 153, 174, 0.2) 15px 15px, rgba(37, 188, 202, 0.1) 20px 20px, rgba(32, 154, 170, 0.05) 25px 25px',
+  }
+}
   return (
     <div className="maincontent"  >
         <div className="headers">
@@ -21,7 +25,9 @@ export default function Main() {
             <p >Always happy to learn new things</p>
         </div>
         <div className="profile" >
-            <motion.div className="cover" whileHover={{boxShadow:'rgba(15, 105, 136, 0.4) 5px 5px, rgba(3, 96, 117, 0.3) 10px 10px, rgba(33, 153, 174, 0.2) 15px 15px, rgba(37, 188, 202, 0.1) 20px 20px, rgba(32, 154, 170, 0.05) 25px 25px'}}><img src={img} alt="" className='box'/></motion.div>
+            <motion.div className="cover" 
+            variants={imgVariants}
+            whileHover="hover"><img src={img} alt="" className='box'/></motion.div>
         </div>
     </div>
   )
