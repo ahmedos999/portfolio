6f60ea8618b6b4c956a4c4a47efef22e02ce2555
ahmed osman <ahmedos099912@gmail.com>
2024-03-26 11:44:59 +0400

    change params

--- src/hooks/useAnalytics.js
+++ src/hooks/useAnalytics.js
@@ -2,7 +2,12 @@ import ReactGA from 'react-ga4'
 
 const useAnalyticsEventTracker = (category="Blog category") => {
   const eventTracker = (action = "test action", label = "test label") => {
-    ReactGA.event({category, action, label});
+    ReactGA.event({
+        category:category,
+        action:action,
+        label:label,
+        value:99
+    });
   }
   return eventTracker;
 }
